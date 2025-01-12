21f80ff98a5d68236a33fdc44053246d6ca2fefa
hoovichen <hoovichen@gmail.com>
2025-01-12 17:25:21 +0800

    conflict again

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   publicPath: process.env.DEPLOY_ENV === 'GH_PAGES' ? '/FDE-Demo/' : '/',
+  outputDir: 'dist',
+  devServer: {
+    historyApiFallback: true, // 确保在开发环境支持路由回退
+  },
   transpileDependencies: [],
-}
+};
